bf0bc62f3ebb63f4b401d11ab857172a845ab964
Michael Savage <msavage@uabmc.edu>
2018-01-24 11:30:44 -0600

    Added option for z or t stack

--- basicFunctions/prepImagingData.m
+++ basicFunctions/prepImagingData.m
@@ -1,12 +1,19 @@
-function [experimentStructure, vol]= prepImagingData(path2ImagingFolder)
+function [experimentStructure, vol]= prepImagingData(path2ImagingFolder, Z_or_TStack)
 % This should read in metadata into experimentStructure and read in imaging
-% files into vol as a pixel x pixel x frame array
+% files into vol as a pixel x pixel x frame array, Z_or_TStack set as 1 for
+% Z stack, set as 2 for T stack
 
 experimentStructure.prairiePath = path2ImagingFolder;
 experimentStructure = prepImagingMetaData(experimentStructure, experimentStructure.prairiePath);
 
 frameFilepath = [experimentStructure.prairiePath experimentStructure.filenamesFrame{1,1}];
-vol = imreadBF(frameFilepath,1,1:length(experimentStructure.filenamesFrame),1);
+
+if Z_or_TStack == 1
+    vol = imreadBF(frameFilepath,1:length(experimentStructure.filenamesFrame),1,1);
+elseif  Z_or_TStack == 2
+    vol = imreadBF(frameFilepath,1,1:length(experimentStructure.filenamesFrame),1);
+end
+
 %StackSlider(vol); % will display the stack of images, need to click
 %greyscale button
 
