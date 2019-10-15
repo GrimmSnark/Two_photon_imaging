f82323f5101799ed3f728da2bd28b45b2fed7e6c
Michael Savage <msavage@uabmc.edu>
2019-10-15 16:20:59 -0500

    Fixes

--- analysis/population_OSI_DSI.m
+++ analysis/population_OSI_DSI.m
@@ -1,21 +1,33 @@
 function [OSI_list, DSI_list] = population_OSI_DSI(filepath, FBSorFISSA, zScoreLimit)
 
+if nargin <3 || isempty(zScoreLimit)
+    zScoreLimit = [];
+end
 
 OSI_list =[];
 DSI_list =[];
 
 filepathList = dir([filepath '\**\*experimentStructure.mat']);
 
-
 for i = 1:length(filepathList)
     load([filepathList(i).folder '\experimentStructure.mat']);
     
-    if FBSorFISSA == 1
-        OSI_list = [OSI_list; experimentStructure.OSI_FBS(experimentStructure.ZScore_FBS>zScoreLimit)];
-        DSI_list = [DSI_list; experimentStructure.DSI_FBS(experimentStructure.ZScore_FBS>zScoreLimit)];
-    elseif FBSorFISSA ==2    
-        OSI_list = [OSI_list; experimentStructure.OSI_FISSA(experimentStructure.ZScore_FISSA>zScoreLimit)];
-        DSI_list = [DSI_list; experimentStructure.DSI_FISSA(experimentStructure.ZScore_FISSA>zScoreLimit)];
-    end
+    if ~isempty(zScoreLimit)
+        if FBSorFISSA == 1
+            OSI_list = [OSI_list; experimentStructure.OSI_FBS(experimentStructure.ZScore_FBS>zScoreLimit)];
+            DSI_list = [DSI_list; experimentStructure.DSI_FBS(experimentStructure.ZScore_FBS>zScoreLimit)'];
+        elseif FBSorFISSA ==2
+            OSI_list = [OSI_list; experimentStructure.OSI_FISSA(experimentStructure.ZScore_FISSA>zScoreLimit)];
+            DSI_list = [DSI_list; experimentStructure.DSI_FISSA(experimentStructure.ZScore_FISSA>zScoreLimit)'];
+        end
         
+    else
+        if FBSorFISSA == 1
+            OSI_list = [OSI_list; experimentStructure.OSI_FBS];
+            DSI_list = [DSI_list; experimentStructure.DSI_FBS'];
+        elseif FBSorFISSA ==2
+            OSI_list = [OSI_list; experimentStructure.OSI_FISSA];
+            DSI_list = [DSI_list; experimentStructure.DSI_FISSA'];
+        end
+    end
 end
